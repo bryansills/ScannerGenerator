693bf230d874374f50acccb8b62c5198f90767e7
David Greenhalgh <david@david-ThinkPad-X220.(none)>
2013-04-09 23:31:29 -0400

    Implemented union operation

--- src/NFAOperations.java
+++ src/NFAOperations.java
@@ -15,7 +15,30 @@ public class NFAOperations {
   	}
 
   	public static NFA union(NFA nfa_a, NFA nfa_b) {
+  		Set<Character> newSet = new Set<Character>(nfa_a.getChars());
+  		newSet.addAll(nfa_b.getChars());
 
+  		//create new nfa to hold unioned nfas
+  		NFA newNFA = new NFA(newSet);
+  		List<NFAState> nextStates = Arrays.asList(nfa_a.getStartState(), nfa_b.getStartState());
+  		
+  		//set transition into a and b to null for empty string
+  		nfa_a.getStartState().setTransition(null);
+  		nfa_b.getStartState().setTransition(null);
+  		newNFA.setStartState(new NFAState(false, new Set<Character>(), nextStates));
+
+  		//create final state
+  		NFAState finalState = new NFAState(true, null, null);
+  		List<NFAState> acceptStates = nfa_a.findAcceptStates();
+  		acceptStates.addAll(nfa_b.findAcceptStates);
+
+  		//point a and b accept states to finalstate
+  		for(NFAState acceptState : acceptStates) {
+	  		acceptState.addNext(finalState);
+	  		acceptState.setAccept(false);
+	  	}
+
+	  	return newNFA;
   	}
 
   	public static NFA star(NFA nfa_a) {
