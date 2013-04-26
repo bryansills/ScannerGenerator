686c3401f6a2217e1cbc680266eed23507df4a52
Jarvis Johnson <magicjarvis@gmail.com>
2013-04-26 00:04:50 -0400

    fixed rdp bug

--- src/ScannerGenerator.java
+++ src/ScannerGenerator.java
@@ -14,7 +14,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-
 public class ScannerGenerator {
 
   public static void main(String[] args) {
@@ -22,7 +21,6 @@ public class ScannerGenerator {
       System.out.println("Usage: java ScannerGenerator <spec-file> <input-file>");
       System.exit(1);
     }
-
 
     try {
       BufferedReader in = new BufferedReader(new FileReader(new File(args[0])));
@@ -39,7 +37,8 @@ public class ScannerGenerator {
 
       in.close();
 
-      Map<String, Set<Character>> classes = CharClasses.buildMapFromSpec(classSpec);
+      Map<String, Set<Character>> classes = CharClasses
+          .buildMapFromSpec(classSpec);
 
       Map<String, NFA> nfas = new HashMap<String, NFA>();
       for (Entry<String, Set<Character>> entry : classes.entrySet()) {
@@ -52,7 +51,6 @@ public class ScannerGenerator {
         regMap.put(temp[0], rd(temp[1], nfas));
       }
 
-
       // read in input file
       in = new BufferedReader(new FileReader(new File(args[1])));
       List<String> inputs = new ArrayList<String>();
@@ -61,39 +59,16 @@ public class ScannerGenerator {
       }
       in.close();
 
-      PrintWriter out = new PrintWriter(new FileWriter(new File("OutputTokens.txt")));
-
+      PrintWriter out = new PrintWriter(new FileWriter(new File(
+          "OutputTokens.txt")));
 
       for (String input : inputs) {
-        // start by splitting by space
         String[] toks = input.split(" ");
         for (String t : toks) {
-          boolean accepted = false;
-          for (Entry<String, NFA> nfa : regMap.entrySet()) {
-            if (nfa.getValue().accepts(t)) {
-              accepted = true;
-              out.println(nfa.getKey().substring(1) + " " + t);
-              break;
-            }
-          }
-          if (!accepted) {
-            while (t.length() > 0) {
-              for (int i = t.length(); i >= 1; i--) {
-                for (Entry<String, NFA> nfa : regMap.entrySet()) {
-                  String sub = t.substring(0, i);
-                  if (nfa.getValue().accepts(sub)) {
-                    out.println(nfa.getKey().substring(1) + " " + sub);
-                    t = t.substring(i);
-                    i = t.length();
-                  }
-                }
-
-              }
-
-            }
-          }
+          walk(t, regMap, out);
         }
       }
+
       out.close();
     } catch (FileNotFoundException e) {
       System.err.println("Can't Find that file");
@@ -107,54 +82,87 @@ public class ScannerGenerator {
     }
   }
 
+  private static void walk(String s, Map<String, NFA> map, PrintWriter out) {
+    if (s.length() > 0) {
+      boolean accepted = false;
+      for (Entry<String, NFA> nfa : map.entrySet()) {
+        if (nfa.getValue().accepts(s)) {
+          out.println(nfa.getKey().substring(1) + " " + s);
+          accepted = true;
+          break;
+        }
+      }
+      if (!accepted) {
+        walk(s.substring(0, 1), map, out);
+        walk(s.substring(1), map, out);
+      }
+    }
+  }
+
   // ($DIGIT|$LOWER)+ $THING* a* b
   // (($DIGIT|$LOWER) ($ASSDF))+
   private static NFA rd(String line, Map<String, NFA> nfas) {
     if (line.length() == 0) {
       return new NFA(null); // empty string transition
     }
+    //System.out.println(line);
     String[] parts = line.split(" ", 2);
     // handle concat
     if (wellFormedBraces(parts[0]) && parts.length > 1) {
+      //System.out.printf("Concat %s %s\n", parts[0], parts[1]);
       return NFAOperations.concat(rd(parts[0], nfas), rd(parts[1], nfas));
     }
-
 
     // we can assume nothing beyond this point has a space in it.
     // TODO(magicjarvis): Make this prettier. I demand it.
     // check for escaped stuff when this works
     int star = line.indexOf('*');
-    if (star != -1 && line.indexOf("\\*") + 1 != star && line.charAt(star - 1) == ')') {
+    if (star != -1 && line.indexOf("\\*") + 1 != star
+        && line.charAt(star - 1) == ')') {
       int start = getStartOfGrouping(line, star - 1);
       if (start == 0) {
         if (star < line.length() - 1) {
-          return NFAOperations.concat(NFAOperations.star(rd(line.substring(start + 1, star - 1), nfas)), rd(line.substring(star + 1), nfas));
+          //System.out.printf("Star %s\n", line.substring(start + 1, star - 1));
+          return NFAOperations
+              .concat(NFAOperations.star(rd(
+                  line.substring(start + 1, star - 1), nfas)),
+                  rd(line.substring(star + 1), nfas));
         } else {
-          return NFAOperations.star(rd(line.substring(start + 1, star - 1), nfas));
+          //System.out.printf("Star %s\n", line.substring(start + 1, star - 1));
+          return NFAOperations.star(rd(line.substring(start + 1, star - 1),
+              nfas));
 
         }
       }
     }
 
     int plus = line.indexOf('+');
-    if (plus != -1 && line.indexOf("\\+") + 1 != plus && line.charAt(plus - 1) == ')') {
+    if (plus != -1 && line.indexOf("\\+") + 1 != plus
+        && line.charAt(plus - 1) == ')') {
       int start = getStartOfGrouping(line, plus - 1);
       if (start == 0) {
         if (plus < line.length() - 1) {
-          return NFAOperations.concat(NFAOperations.star(rd(line.substring(start + 1, plus - 1), nfas)), rd(line.substring(plus + 1), nfas));
+          //System.out.printf("Plus %s\n", line.substring(start + 1, plus - 1));
+          return NFAOperations
+              .concat(NFAOperations.star(rd(
+                  line.substring(start + 1, plus - 1), nfas)),
+                  rd(line.substring(plus + 1), nfas));
         } else {
-          return NFAOperations.plus(rd(line.substring(start + 1, plus - 1), nfas));
+          return NFAOperations.plus(rd(line.substring(start + 1, plus - 1),
+              nfas));
         }
       }
     }
 
     String[] unionParts = line.split("\\|", 2);
     if (wellFormedBraces(unionParts[0]) && unionParts.length > 1) {
-      return NFAOperations.union(rd(unionParts[0], nfas), rd(unionParts[1], nfas));
+      //System.out.printf("Union %s %s\n", unionParts[0], unionParts[1]);
+      return NFAOperations.union(rd(unionParts[0], nfas),
+          rd(unionParts[1], nfas));
     }
 
     // check if one level deep ()s
-    //if (line.charAt(index))
+    // if (line.charAt(index))
     int lastParen = line.lastIndexOf(')');
     if (lastParen != -1 && line.lastIndexOf("\\(") + 1 != lastParen) {
       int start = getStartOfGrouping(line, lastParen);
@@ -163,13 +171,17 @@ public class ScannerGenerator {
         if (lastParen == line.length() - 1) {
           return rd(grouping, nfas);
         } else {
-          return NFAOperations.concat(rd(grouping, nfas), rd(line.substring(lastParen + 1), nfas));
+          //System.out.printf("Concat %s %s\n", grouping, line.substring(lastParen + 1));
+          return NFAOperations.concat(rd(grouping, nfas),
+              rd(line.substring(lastParen + 1), nfas));
         }
       }
     }
 
     if (wellFormedBraces(unionParts[0]) && unionParts.length > 1) {
-      return NFAOperations.union(rd(unionParts[0], nfas), rd(unionParts[1], nfas));
+      //System.out.printf("Union %s %s\n", unionParts[0], line.substring(lastParen + 1));
+      return NFAOperations.union(rd(unionParts[0], nfas),
+          rd(unionParts[1], nfas));
     }
 
     // handle plus
@@ -184,7 +196,9 @@ public class ScannerGenerator {
           open--;
         }
         if (open == 0) {
-          return NFAOperations.concat(rd(line.substring(1, i), nfas), rd(line.substring(i+1), nfas));
+          //System.out.printf("Concat %s %s\n", line.substring(1, i), line.substring(i + 1));
+          return NFAOperations.concat(rd(line.substring(1, i), nfas),
+              rd(line.substring(i + 1), nfas));
         }
       }
     }
@@ -197,32 +211,39 @@ public class ScannerGenerator {
         if (line.length() == 1) {
           return new NFA(ascii);
         } else {
-          return NFAOperations.concat(new NFA(ascii), rd(line.substring(1), nfas));
+          return NFAOperations.concat(new NFA(ascii),
+              rd(line.substring(1), nfas));
         }
       }
 
       Set<Character> singletonSet = singletonSet(first);
 
       if (first == '\\' && line.length() > 1) {
+        //System.out.println("Found an escaped char");
         singletonSet = singletonSet(line.charAt(1));
+        //System.out.println("Created this singleton set " + singletonSet);
       }
 
       NFA oneChar = new NFA(singletonSet);
       if (line.length() == 1 || (line.length() == 2 && first == '\\')) {
+        //System.out.println("NFA " + line);
         return oneChar;
       }
-      return NFAOperations.concat(oneChar, rd(line.substring(1), nfas));
+      char temp = (first == '\\') ? line.charAt(1) : first;
+      int sub = (first == '\\') ? 2 : 1;
+      //System.out.printf("Concat %s %s\n", ""+temp, line.substring(sub));
+      return NFAOperations.concat(oneChar, rd(line.substring(sub), nfas));
     } else if (nfas.containsKey(line)) {
+      //System.out.println("We have this " + line);
       return nfas.get(line);
     }
 
     return null;
 
-
   }
 
   public static Set<Character> singletonSet(Character c) {
-    return new HashSet<Character>(Arrays.asList(new Character[]{c}));
+    return new HashSet<Character>(Arrays.asList(new Character[] { c }));
   }
 
   public static boolean wellFormedBraces(String line) {
@@ -240,6 +261,7 @@ public class ScannerGenerator {
     }
     return open == 0;
   }
+
   public static int getStartOfGrouping(String line, int closing) {
     int open = 1;
     char[] chars = line.toCharArray();
